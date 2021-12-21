b21b66e69fd1aeb460cbe3e9ed5f3a260e84ee3c
Dem01isher <madarthas445@gmail.com>
2021-12-22 00:23:03 +0200

    complete config

---
+++ app/src/main/java/com/github/demolisher/animedb/views/application/AnimeApp.kt
@@ -0,0 +1,12 @@
+package com.github.demolisher.animedb.views.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ *  Created by Android Studio on 12/20/2021 1:07 PM
+ *  Developer: Sergey Leskov
+ */
+
+@HiltAndroidApp
+class AnimeApp : Application()
